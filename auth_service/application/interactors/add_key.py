2d21e9b397abab715a613368dfb46e9aaa85a898
InWamos <mykyta.pidgornyi@gmail.com>
2025-06-02 22:28:54 +0300

    merge add key into one

---
+++ auth_service/application/interactors/add_key.py
@@ -0,0 +1,14 @@
+import datetime
+from dataclasses import dataclass
+
+from auth_service.domain.api_key import APIKeyAccessLevelEnum, APIKeyStatusEnum
+
+
+# Slots makes it impossible for this dataclass to obtain any new attributes
+@dataclass(frozen=True, slots=True)
+class AddKeyDTO:
+    key: str
+    access_level: APIKeyAccessLevelEnum
+    status: APIKeyStatusEnum
+    created_at: datetime
+    last_accessed: datetime
