d2be6e5d4aa5c40e4ea5de0b3ab4ac8bfe292ed8
InWamos <mykyta.pidgornyi@gmail.com>
2025-06-03 22:56:29 +0300

    add key interactor init

--- auth_service/application/interactors/add_key.py
+++ auth_service/application/interactors/add_key.py
@@ -1,14 +1,43 @@
 from dataclasses import dataclass
 from datetime import datetime
 
-from auth_service.domain.api_key import APIKeyAccessLevelEnum, APIKeyStatusEnum
+from application.abstract.database.repositories.api_key_repository import (
+    ApiKeyWriterRepository,
+)
+from application.abstract.database.secret_generator import (
+    SecretGenerator,
+)
+from application.abstract.database.transaction_manager import (
+    TransactionManager,
+)
+from application.abstract.interactor import Interactor
+from domain.api_key import APIKey, APIKeyAccessLevelEnum, APIKeyStatusEnum
 
 
 # Slots makes it impossible for this dataclass to obtain any new attributes
 @dataclass(frozen=True, slots=True)
 class AddKeyDTO:
-    key: str
     access_level: APIKeyAccessLevelEnum
     status: APIKeyStatusEnum
     created_at: datetime
     last_accessed: datetime
+
+
+@dataclass(frozen=True, slots=True)
+class AddKeyResultDTO:
+    key: str
+
+
+class AddKey(Interactor[AddKeyDTO, None]):
+    def __init__(
+        self,
+        api_key_writer: ApiKeyWriterRepository,
+        transaction_manager: TransactionManager,
+        secret_generator: SecretGenerator,
+    ) -> None:
+        self._api_key_writer = api_key_writer
+        self._transaction_manager = transaction_manager
+        self._secret_generator = secret_generator
+
+    def __call__(self, data: AddKeyDTO) -> None:
+        ...
