f5db1dc48d7e5b48a344ecec50187418aa7977d2
InWamos <mykyta.pidgornyi@gmail.com>
2025-06-15 22:43:37 +0300

    fix generic arguments

--- auth_service/domain/services/api_key.py
+++ auth_service/domain/services/api_key.py
@@ -1,9 +1,12 @@
 from domain.ports.api_key_encryption import APIKeyEncryption
 from domain.ports.api_key_generator import APIKeyGenerator
+from domain.ports.api_key_type import SecretKey
 
 
 class APIKeyService:
     def __init__(
-        self, key_generator: APIKeyGenerator, key_encryption: APIKeyEncryption,
+        self,
+        key_generator: APIKeyGenerator[SecretKey],
+        key_encryption: APIKeyEncryption[SecretKey],
     ) -> None:
         pass
