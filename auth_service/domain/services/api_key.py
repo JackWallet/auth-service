0ddbbb94138c478723be3d134373b44c5825d7f7
InWamos <mykyta.pidgornyi@gmail.com>
2025-06-16 21:41:11 +0300

    add create method

--- auth_service/domain/services/api_key.py
+++ auth_service/domain/services/api_key.py
@@ -1,3 +1,10 @@
+from datetime import UTC, datetime
+
+from domain.entities.api_key import (
+    APIKey,
+    APIKeyAccessLevelEnum,
+    APIKeyStatusEnum,
+)
 from domain.ports.api_key_encryption import APIKeyEncryption
 from domain.ports.api_key_generator import APIKeyGenerator
 from domain.ports.api_key_type import SecretKey
@@ -9,4 +16,17 @@ class APIKeyService:
         key_generator: APIKeyGenerator[SecretKey],
         key_encryption: APIKeyEncryption[SecretKey],
     ) -> None:
-        pass
+        self._key_generator = key_generator
+        self._key_encryption = key_encryption
+
+    def create(self, access_level: APIKeyAccessLevelEnum) -> APIKey:
+        now = datetime.now(tz=UTC)
+        key = self._key_generator.generate_key()
+        return APIKey(
+            key_id=None,
+            key=str(key),
+            access_level=access_level,
+            status=APIKeyStatusEnum.ACTIVE,
+            created_at=now,
+            last_accessed=now,
+        )
