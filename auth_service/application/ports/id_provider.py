cc735c0a0e0d1c91772d04558b87c55046075191
InWamos <mykyta.pidgornyi@gmail.com>
2025-06-21 23:31:24 +0300

    add raising not implemented error

--- auth_service/application/ports/id_provider.py
+++ auth_service/application/ports/id_provider.py
@@ -7,4 +7,5 @@ InputDTO = TypeVar("InputDTO")
 
 class IdProvider(ABC, Generic[InputDTO, OutputDTO]):
     @abstractmethod
-    def get_user_acknowledgements(self, data: InputDTO) -> OutputDTO: ...
+    def get_user_acknowledgements(self, data: InputDTO) -> OutputDTO:
+        raise NotImplementedError
