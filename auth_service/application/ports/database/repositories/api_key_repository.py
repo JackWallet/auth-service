8895e95652e092b48522f47fcceaa7bf08986b6e
InWamos <mykyta.pidgornyi@gmail.com>
2025-06-21 23:59:58 +0300

    fix argument name

--- auth_service/application/ports/database/repositories/api_key_repository.py
+++ auth_service/application/ports/database/repositories/api_key_repository.py
@@ -6,7 +6,7 @@ from domain.entities.api_key import APIKey
 
 class ApiKeyReaderRepository(Protocol):
     @abstractmethod
-    async def get_key_by_key_value(self, key_id: str) -> APIKey:
+    async def get_key_by_key_value(self, key_value: str) -> APIKey:
         raise NotImplementedError
 
 
