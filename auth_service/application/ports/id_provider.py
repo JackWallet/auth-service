f62e24c3dfaeb88eb3eaba7723fa51c10e240e32
InWamos <mykyta.pidgornyi@gmail.com>
2025-06-21 23:47:05 +0300

    move Id provider to separate module

--- auth_service/application/ports/id_provider.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class IdProvider(ABC):
-    @abstractmethod
-    async def get_user_acknowledgements(
-        self, data: dict[str, str],
-    ) -> dict[str, str]:
-        raise NotImplementedError
