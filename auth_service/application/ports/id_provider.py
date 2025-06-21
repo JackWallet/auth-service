1cfb71a8987ee60cd5c91edc08c4c4eb58deeff9
InWamos <mykyta.pidgornyi@gmail.com>
2025-06-21 23:28:43 +0300

    id provider interface

---
+++ auth_service/application/ports/id_provider.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+OutputDTO = TypeVar("OutputDTO")
+InputDTO = TypeVar("InputDTO")
+
+
+class IdProvider(ABC, Generic[InputDTO, OutputDTO]):
+    @abstractmethod
+    def get_user_acknowledgements(self, data: InputDTO) -> OutputDTO: ...
