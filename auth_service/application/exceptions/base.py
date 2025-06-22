0209e40f9db79969c48fb5857c519a830ce676c5
InWamos <mykyta.pidgornyi@gmail.com>
2025-06-22 16:21:42 +0300

    add base auth errors

--- auth_service/application/exceptions/base.py
+++ auth_service/application/exceptions/base.py
@@ -1,2 +1,18 @@
 class ApplicationError(Exception):
     pass
+
+
+class InsufficientPrivilegesError(ApplicationError):
+    pass
+
+
+class AuthenticationError(InsufficientPrivilegesError):
+    pass
+
+
+class ApiKeyExpiredError(AuthenticationError):
+    pass
+
+
+class ApiKeyRevokedError(AuthenticationError):
+    pass
