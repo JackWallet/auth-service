2416dedb062de7c850f200e9365ca249b932dcad
InWamos <mykyta.pidgornyi@gmail.com>
2025-06-22 16:36:29 +0300

    add api key validators

--- auth_service/application/access_level_validator.py
+++ auth_service/application/access_level_validator.py
@@ -1,6 +1,24 @@
-from domain.entities.api_key import APIKeyAccessLevelEnum
+from application.exceptions.base import (
+    ApiKeyExpiredError,
+    ApiKeyRevokedError,
+    InsufficientPrivilegesError,
+)
+from domain.entities.api_key import APIKeyAccessLevelEnum, APIKeyStatusEnum
 
 
 def validate_api_key_access_level(
-    input: APIKeyAccessLevelEnum, target: APIKeyAccessLevelEnum,
-) -> None: ...
+    input_access_level: APIKeyAccessLevelEnum,
+    target_access_level: APIKeyAccessLevelEnum,
+) -> None:
+    if (
+        input_access_level is APIKeyAccessLevelEnum.WRITE
+        and target_access_level is APIKeyAccessLevelEnum.READ
+    ):
+        raise InsufficientPrivilegesError
+
+
+def validate_api_key_status(api_key_status: APIKeyStatusEnum) -> None:
+    if api_key_status is APIKeyStatusEnum.EXPIRED:
+        raise ApiKeyExpiredError
+    if api_key_status is APIKeyStatusEnum.REVOKED:
+        raise ApiKeyRevokedError
