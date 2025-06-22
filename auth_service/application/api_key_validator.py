db384f59da4fa9a5e9ed42e57757a6468fe35477
InWamos <mykyta.pidgornyi@gmail.com>
2025-06-22 16:39:18 +0300

    add separate class for value validation

--- auth_service/application/api_key_validator.py
+++ auth_service/application/api_key_validator.py
@@ -6,19 +6,21 @@ from application.exceptions.base import (
 from domain.entities.api_key import APIKeyAccessLevelEnum, APIKeyStatusEnum
 
 
-def validate_api_key_access_level(
-    input_access_level: APIKeyAccessLevelEnum,
-    target_access_level: APIKeyAccessLevelEnum,
-) -> None:
-    if (
-        input_access_level is APIKeyAccessLevelEnum.WRITE
-        and target_access_level is APIKeyAccessLevelEnum.READ
-    ):
-        raise InsufficientPrivilegesError
+class ApiKeyValidator:
+    @staticmethod
+    def validate_api_key_access_level(
+        input_access_level: APIKeyAccessLevelEnum,
+        target_access_level: APIKeyAccessLevelEnum,
+    ) -> None:
+        if (
+            input_access_level is APIKeyAccessLevelEnum.WRITE
+            and target_access_level is APIKeyAccessLevelEnum.READ
+        ):
+            raise InsufficientPrivilegesError
 
-
-def validate_api_key_status(api_key_status: APIKeyStatusEnum) -> None:
-    if api_key_status is APIKeyStatusEnum.EXPIRED:
-        raise ApiKeyExpiredError
-    if api_key_status is APIKeyStatusEnum.REVOKED:
-        raise ApiKeyRevokedError
+    @staticmethod
+    def validate_api_key_status(api_key_status: APIKeyStatusEnum) -> None:
+        if api_key_status is APIKeyStatusEnum.EXPIRED:
+            raise ApiKeyExpiredError
+        if api_key_status is APIKeyStatusEnum.REVOKED:
+            raise ApiKeyRevokedError
