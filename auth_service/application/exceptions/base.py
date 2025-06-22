4ebf4210414744a2fd3f072e5a1931ad8f27d0e8
InWamos <mykyta.pidgornyi@gmail.com>
2025-06-22 17:18:40 +0300

    add message to errors

--- auth_service/application/exceptions/base.py
+++ auth_service/application/exceptions/base.py
@@ -3,7 +3,11 @@ class ApplicationError(Exception):
 
 
 class InsufficientPrivilegesError(ApplicationError):
-    pass
+    def __init__(self, access_level_required: str) -> None:
+        super().__init__(
+            f"You need {access_level_required} \
+                access level to perform this operation",
+        )
 
 
 class AuthenticationError(ApplicationError):
@@ -11,12 +15,21 @@ class AuthenticationError(ApplicationError):
 
 
 class ApiKeyExpiredError(AuthenticationError):
-    pass
+    def __init__(self, api_key_raw: str) -> None:
+        super().__init__(
+            f"Api Key {api_key_raw} has been expired",
+        )
 
 
 class ApiKeyRevokedError(AuthenticationError):
-    pass
+    def __init__(self, api_key_raw: str) -> None:
+        super().__init__(
+            f"Api Key {api_key_raw} has been revoked",
+        )
 
 
 class ApiKeyNotFoundError(AuthenticationError):
-    pass
+    def __init__(self, api_key_raw: str) -> None:
+        super().__init__(
+            f"Api Key {api_key_raw} doesn't exist",
+        )
