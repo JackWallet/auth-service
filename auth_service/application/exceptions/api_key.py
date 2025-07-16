7cafc3f45392a80f6601e8b8c380fe52fb3eb6ed
InWamos <mykyta.pidgornyi@gmail.com>
2025-07-16 23:42:40 +0300

    remove unused errors

--- auth_service/application/exceptions/api_key.py
+++ auth_service/application/exceptions/api_key.py
@@ -1,17 +1,7 @@
 from application.exceptions.base import ApplicationError
 
 
-class ApiKeyOperationError(ApplicationError):
-    pass
-
-
 class ApiKeyNotFoundError(ApplicationError):
     pass
 
 
-class ApiKeyRevokedError(ApplicationError):
-    pass
-
-
-class ApiKeyExpiredError(ApplicationError):
-    pass
